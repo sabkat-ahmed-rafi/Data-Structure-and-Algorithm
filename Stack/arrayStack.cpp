70eb1a82c47c959cb855802f9c1369e10f01f101
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-11-05 19:27:23 +0600

    Stack created with array

---
+++ Stack/arrayStack.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+#define MAX 5
+
+class ArrayStack {
+    int top;
+    int arr[MAX];
+
+public:
+    ArrayStack() {
+        top = -1;
+    }
+
+
+    bool push(int x) {
+        if(top >= MAX - 1) {
+            return false;
+        } else {
+            arr[++top] = x;
+        }
+    }
+
+    int pop() {
+        if(top < 0) {
+            return 0;
+        } else {
+            return arr[top--];
+        }
+    }
+
+    int peek() {
+        return arr[top];
+    }
+
+    void display() {
+        for(int i = top; i >= 0; i--) {
+            cout<< arr[i] << ", ";
+        }
+    }
+};
+
+
+int main() {
+    ArrayStack stack;
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);
+    stack.push(40);
+    stack.pop();
+
+    cout<< "top element in stack: "<< stack.peek() << endl;
+    stack.display();
+
+    return 0;
+}
