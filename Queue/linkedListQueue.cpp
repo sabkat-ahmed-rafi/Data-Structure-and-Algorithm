1ee19fcf61f71374845e8d67be4220593823d985
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-11-07 14:11:24 +0600

    linked list queue created

--- Queue/linkedListQueue.cpp
+++ Queue/linkedListQueue.cpp
@@ -27,10 +27,48 @@ public:
         return head == nullptr;
     }
 
-    void 
+    void enqueue(int value) {
+        Node* newNode = new Node(value);
+        if(isEmpty()) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void dequeue() {
+        if(isEmpty()) {
+            return;
+        }
+        Node* temp = head;
+        head = head->next;
+        if(head == nullptr) {
+            tail = nullptr;
+        }
+        delete temp;
+    }
+
+    int getFront() {
+        if(isEmpty()) {
+            return -1;
+        }
+        return head->data;
+    }
 };
 
 
 int main() {
+    Queue queue;
+
+    queue.enqueue(10);
+    queue.enqueue(20);
+    queue.enqueue(30);
+    queue.enqueue(40);
+
+    while(!queue.isEmpty()) {
+        cout<< queue.getFront() << " ,";
+        queue.dequeue();
+    }
 
 }
