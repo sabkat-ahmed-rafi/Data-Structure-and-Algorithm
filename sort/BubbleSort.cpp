385098b8c00034108a3543c5c78429d4f2749bb5
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-11-17 11:18:09 +0600

    bubble sort implemented

---
+++ sort/BubbleSort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int size) {
+    for(int i = 0; i < size - 1; i++) {
+        for(int j = 0; j < size - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, size);
+    printArray(arr, size);
+    return 0;
+}
