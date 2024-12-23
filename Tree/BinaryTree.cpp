18e3d0b56188635501d0c1c674cc10ebc36d0f01
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-12-23 14:52:37 +0600

    Binary tree explored

---
+++ Tree/BinaryTree.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+
+void inOrder(Node* root) {
+    if(root == nullptr) 
+        return;
+
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+
+
+int main() {
+
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    inOrder(root);
+
+    return 0;
+}
