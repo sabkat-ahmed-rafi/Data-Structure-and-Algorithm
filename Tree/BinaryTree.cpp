44aa7195811f6b3d661146ecfbbd95942d9bb69f
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-12-26 19:56:36 +0600

    binary tree

--- Tree/BinaryTree.cpp
+++ Tree/BinaryTree.cpp
@@ -24,6 +24,28 @@ void inOrder(Node* root) {
     inOrder(root->right);
 }
 
+// maxDepth and height is similar
+int maxDepth(Node* root) {
+    if(root == nullptr) return 0;
+    return 1 + max(maxDepth(root->left), maxDepth(root->right));
+}
+
+int minDepth(Node* root) {
+    if(root == nullptr) return 0;
+    if(root->left == nullptr) return 1 + minDepth(root->right);
+    if(root->right == nullptr) return 1 + minDepth(root->left);
+    return min(minDepth(root->left), minDepth(root->right));
+}
+
+
+bool isBalanced(Node* root) {
+    if(root == nullptr) return true;
+
+    int lh =  maxDepth(root->left);
+    int rh = maxDepth(root->right);
+
+    return abs(lh - rh) <= 1 && isBalanced(root->left) && isBalanced(root->right);
+}
 
 
 int main() {
@@ -34,7 +56,12 @@ int main() {
     root->left->left = new Node(4);
     root->left->right = new Node(5);
 
+
     inOrder(root);
+    cout << endl;
+    cout << maxDepth(root) << endl;
+    cout << minDepth(root) << endl;
+    cout << isBalanced(root);
 
     return 0;
 }
