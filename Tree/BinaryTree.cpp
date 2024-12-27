18986a1de6c469805b676b80e7898f34ec29189e
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-12-28 00:32:01 +0600

    binary tree

--- Tree/BinaryTree.cpp
+++ Tree/BinaryTree.cpp
@@ -34,7 +34,7 @@ int minDepth(Node* root) {
     if(root == nullptr) return 0;
     if(root->left == nullptr) return 1 + minDepth(root->right);
     if(root->right == nullptr) return 1 + minDepth(root->left);
-    return min(minDepth(root->left), minDepth(root->right));
+    return 1 + min(minDepth(root->left), minDepth(root->right));
 }
 
 
