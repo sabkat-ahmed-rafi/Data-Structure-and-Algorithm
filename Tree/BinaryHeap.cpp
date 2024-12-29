7979bcf00584e1b4d28b41b20a16ab69e694d990
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-12-29 20:14:01 +0600

    binary heap

--- Tree/BinaryHeap.cpp
+++ Tree/BinaryHeap.cpp
@@ -16,11 +16,30 @@ private:
             }
         }
     }
+    void heapify(int index) {
+        int largest = index;
+        int left = (2 * index) + 1;
+        int right = (2 * index) + 2;
+
+        if(left < heap.size() && heap[left] > heap[largest]) largest = left;
+        if(right < heap.size() && heap[right] > heap[largest]) largest = right;
+
+        if(largest != index) {
+            swap(heap[index], heap[largest]);
+            heapify(largest);
+        }
+    }
 
 public:
     void insert(int value) {
         heap.push_back(value);
         bubbleUp(heap.size() - 1);
+    }
+    void deleteRoot() {
+        if(heap.empty()) return;
+        heap[0] = heap.back();
+        heap.pop_back();
+        heapify(0);
     }
     void printHeap() {
         for(int val : heap) cout << val << " ";
@@ -35,7 +54,7 @@ int main() {
     binaryHeap.insert(40);
     binaryHeap.insert(30);
     binaryHeap.insert(60);
-
+    binaryHeap.deleteRoot();
     binaryHeap.printHeap();
 
     return 0;
